980c8661f76309487e507942e5035d054004e67b
Lukacs J Papp <papplukacs@hotmail.com>
2023-11-05 22:14:13 +0000

    input is good

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,7 +5,7 @@ export default function SearchBar() {
   return (
     <Input
     aria-label="Search"
-    className="w-full max-w-md px-2 md:px-4 py-1 md:py-2 border border-gray-300 mb-5 rounded-md shadow-sm placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    className="w-10/12 max-w-md px-2 md:px-4 py-5 md:py-5 border border-gray-300 mb-5 rounded-md shadow-sm placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
     placeholder="Search By Ingridients, Cuisine or Calories"
     type="search"
   />
