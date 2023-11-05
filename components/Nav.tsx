8053475d57087cda19388ad2fb9faf877f691550
Lukacs J Papp <papplukacs@hotmail.com>
2023-11-05 22:02:12 +0000

    nav is styled

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,14 +4,8 @@ import { Button } from "./ui/button";
 export default function Nav() {
   return (
     <header>
-      <nav className="w-full z-10 py-2 px-4 md:px-6 lg:px-8">
-        <div className="container mx-auto flex items-center justify-between">
-          <div>
-            <Button className="text-sm md:text-base" variant="outline">
-              Home
-            </Button>
-          </div>
-          <div className="flex space-x-4">
+      <nav className="bg-white dark:bg-gray-800 shadow w-full z-10 py-2 px-4 md:px-6 lg:px-8">
+        <div className="container mx-auto flex items-center justify-evenly">
             <Button className="text-sm md:text-base" variant="outline">
               About
             </Button>
@@ -21,7 +15,6 @@ export default function Nav() {
             <Button className="text-sm md:text-base" variant="outline">
               Contact
             </Button>
-          </div>
         </div>
       </nav>
     </header>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,8 +5,8 @@ export default function SearchBar() {
   return (
     <Input
     aria-label="Search"
-    className="w-full max-w-md px-2 md:px-4 py-1 md:py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
-    placeholder="Search for articles..."
+    className="w-full max-w-md px-2 md:px-4 py-1 md:py-2 border border-gray-300 mb-5 rounded-md shadow-sm placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    placeholder="Search By Ingridients, Cuisine or Calories"
     type="search"
   />
   )
