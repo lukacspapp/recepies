88d5c0514c913ebb6bd6f327b4159e17bc849c6c
Lukacs J Papp <papplukacs@hotmail.com>
2023-11-15 20:53:53 +0000

    nav added

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,6 +9,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { NavigationMenuDemo } from "./Navigation";
 
 
 export default function Nav() {
@@ -17,17 +18,19 @@ export default function Nav() {
 
   return (
     <header>
-      <nav className="bg-white dark:bg-gray-800 shadow w-full z-10 py-2 px-2 md:px-4 lg:px-6">
-        <div className="container mx-auto flex items-center justify-between">
-          <div>
-            <Link href="/" className="text-xl font-bold text-gray-800 dark:text-white md:text-2xl hover:text-gray-700 dark:hover:text-gray-300">
-              🥧
-            </Link>
-          </div>
+    <nav className="bg-white dark:bg-gray-800 shadow w-full z-10 py-2 px-2 md:px-4 lg:px-6">
+      <div className="container mx-auto flex items-center justify-between">
+        <div>
+          <Link href="/" className="text-xl font-bold text-gray-800 dark:text-white md:text-2xl hover:text-gray-700 dark:hover:text-gray-300">
+            🥧
+          </Link>
+        </div>
+        <div className="md:hidden">
+          {/* Burger icon and dropdown menu for mobile */}
           <DropdownMenu>
             <DropdownMenuTrigger>
               <svg
-                className="h-6 w-6"
+                className="h-6 w-6 cursor-pointer"
                 fill="none"
                 height="24"
                 stroke="currentColor"
@@ -37,6 +40,7 @@ export default function Nav() {
                 viewBox="0 0 24 24"
                 width="24"
                 xmlns="http://www.w3.org/2000/svg"
+                onClick={() => setIsOpen(!isOpen)}
               >
                 <line x1="4" x2="20" y1="12" y2="12" />
                 <line x1="4" x2="20" y1="6" y2="6" />
@@ -49,24 +53,25 @@ export default function Nav() {
               <DropdownMenuItem>Vegan</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-          <div className="md:hidden relative" onClick={() => setIsOpen(!isOpen)}>
-
-          </div>
         </div>
-      </nav>
-      {isOpen && (
-        <div className="md:flex md:space-x-4">
-          <Button className="text-xs md:text-sm lg:text-base" variant="outline">
-            About
-          </Button>
-          <Button className="text-xs md:text-sm lg:text-base" variant="outline">
-            Services
-          </Button>
-          <Button className="text-xs md:text-sm lg:text-base" variant="outline">
-            Contact
-          </Button>
+        <div className="hidden md:flex md:space-x-4">
+        <NavigationMenuDemo />
         </div>
-      )}
-    </header>
+      </div>
+    </nav>
+    {isOpen && (
+      <div className="md:flex md:space-x-4">
+        <Button className="text-xs md:text-sm lg:text-base" variant="outline">
+          About
+        </Button>
+        <Button className="text-xs md:text-sm lg:text-base" variant="outline">
+          Services
+        </Button>
+        <Button className="text-xs md:text-sm lg:text-base" variant="outline">
+          Contact
+        </Button>
+      </div>
+    )}
+  </header>
   )
 }
