6301d3100e53fee6332425c0e54c5b21ed49d656
Lukacs J Papp <papplukacs@hotmail.com>
2023-11-17 13:07:30 +0000

    categories are done

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -1,9 +1,31 @@
-export default async function page({params}: {params: {slug: string}}) {
+import AnimatedDescription from "@/components/AnimatedDescription"
+import CategoryCard from "@/components/CategoryCard";
+import { doRequest } from "@/lib/DoRequest"
 
-  const { slug } = params
+export default async function page({ params }: { params: { slug: string } }) {
+
+  const { meals } = await doRequest('GET', `${process.env.RECEPIES_API_NAME + params.slug.replace(/-/g, '_')}`);
 
 
   return (
-    <div>{slug}</div>
+    <main className="w-full">
+      <section className="w-full py-12 md:py-24 lg:py-32">
+        <div className="container px-4 md:px-6">
+          <AnimatedDescription
+            title={params.slug.charAt(0).toUpperCase() + params.slug.slice(1).replace(/-/g, ' ')}
+            description={`Best Recepies within the ${params.slug.charAt(0).toUpperCase() + params.slug.slice(1).replace(/-/g, ' ')} Category` }
+          />
+          <div className="grid gap-10 sm:gap-12 md:gap-16 md:grid-cols-2 lg:grid cols-2 lg:gap-8 xl:grid-cols-3 2xl:grid-cols-4">
+            {meals.map((meal: any) => (
+              <CategoryCard
+                key={meal.idMeal}
+                name={meal.strMeal}
+                image={meal.strMealThumb}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -7,12 +7,14 @@ import Link from 'next/link'
 type CategoryCardProps = {
   name: string
   image: string
-  description: string
+  description?: string
 }
 
-export default function CategoryCard({ name, image, description }: CategoryCardProps) {
+export default function CategoryCard({ name, image, description = '' }: CategoryCardProps) {
   return (
-    <div className="space-y-2 p-4 bg-white shadow-lg rounded-xl transform transition-transform hover:scale-105">
+    <div
+      onClick={() => { if (!description) window.location.href = `/${name.toLowerCase().replace(/ /g, '-')}`; }}
+      className={`${!description ? ' cursor-pointer' : ''} space-y-2 p-4 bg-white shadow-lg rounded-xl transform transition-transform hover:scale-105`}>
       <div className="rounded-xl overflow-hidden">
         <Image
           alt={name}
@@ -24,18 +26,18 @@ export default function CategoryCard({ name, image, description }: CategoryCardP
         <h2 className="leading-tighter text-xl font-bold tracking-tighter sm:text-2xl md:text-3xl">
           {name}
         </h2>
-        <div className="relative h-[6rem] overflow-hidden">
+        {description && <div className="relative h-[6rem] overflow-hidden">
           <p className="absolute top-0 left-0 max-w-[700px] text-zinc-500 md:text-lg dark:text-zinc-400">
             {description}
           </p>
           <div className="absolute bottom-0 left-0 w-full h-[5rem] bg-gradient-to-t from-white dark:from-zinc-800" />
-        </div>
-        <Link
+        </div>}
+        {description && <Link
           className="w-full flex items-center justify-start text-center text-zinc-700 dark:text-zinc-300 font-semibold hover:text-zinc-400 dark:hover:text-zinc-600 transition-colors duration-200"
-          href={`categories/${name.toLowerCase().replace(/ /g, '-')}`}
+          href={`/${name.toLowerCase().replace(/ /g, '-')}`}
         >
           Read More
-        </Link>
+        </Link>}
       </div>
     </div>
   )
