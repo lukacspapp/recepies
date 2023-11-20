832b9e68336d608d7ff738e31a4b883affadd8f4
Lukacs J Papp <papplukacs@hotmail.com>
2023-11-20 18:01:42 +0000

    statrted

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,12 +4,15 @@ import React from 'react'
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container">
+    <main className="container mt-11">
+      <AnimatedDescription title={"Dashboard"} description={"Welcome to your dashboard"} />
+      <div className='flex items-stretch'>
         <aside className="hidden w-[200px] flex-col md:flex">
-          <DashboardNav  />
+          <DashboardNav />
         </aside>
-        {children}
+        <h1>Hello</h1>
       </div>
+    </main>
 
   )
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,10 @@
-import AnimatedDescription from '@/components/AnimatedDescription'
-import { DashboardNav } from '@/components/DashboardNav'
+
 import React from 'react'
 
 export default function page() {
   return (
-    <main className="w-full">
-      <section className="w-full container py-12 md:py-24 lg:py-32">
-        <AnimatedDescription title={"Dashboard"} description={"Welcome to your dashboard"}/>
-      </section>
+    <main>
+      <h1>Meals comes here</h1>
     </main>
-
   )
 }
