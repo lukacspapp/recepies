07a632f32bf705daf725ad6e5487ea5381bc0af6
Lukacs J Papp <papplukacs@hotmail.com>
2023-12-06 12:50:59 +0000

    added icons

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -21,8 +21,6 @@ export default function RegisterPage() {
       >
         Login
       </Link>
-      <div className="hidden h-full bg-muted lg:block" />
-      <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -34,8 +32,6 @@ export default function RegisterPage() {
           </div>
           <UserAuthForm />
         </div>
-      </div>
     </div>
-
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Providers } from '@/components/Provider'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { AuthProvider } from '@/context/Auth';
+import log from '.'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +15,7 @@ export const metadata = {
     template: "%s | Recipes from Around the World",
   },
   description:
-  "Delicious Recipes Hub is your go-to destination for discovering and searching a diverse range of recipes. Find inspiration for your next culinary creation as you search, cook, and share delightful meals from around the world. Elevate your cooking experience and satisfy your taste buds with our extensive collection of recipes. Start your flavorful journey today!",
+    "Delicious Recipes Hub is your go-to destination for discovering and searching a diverse range of recipes. Find inspiration for your next culinary creation as you search, cook, and share delightful meals from around the world. Elevate your cooking experience and satisfy your taste buds with our extensive collection of recipes. Start your flavorful journey today!",
   keywords: [
     "recipes",
     "cooking",
@@ -32,17 +33,35 @@ export const metadata = {
   openGraph: {
     type: "website",
     locale: "en_US",
-    url: "https://recepies.org",
+    url: "https://recepies.lukacsjpapp.com/",
     title: "Recepies",
     description: "Delicious Recipes Hub is your go-to destination for discovering and searching a diverse range of recipes. Find inspiration for your next culinary creation as you search, cook, and share delightful meals from around the world. Elevate your cooking experience and satisfy your taste buds with our extensive collection of recipes. Start your flavorful journey today!",
     siteName: "Recepies",
   },
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
+    icon: "./favicon.ico",
+    shortcut: "../public/favicon-16x16.png",
+    apple: "../public/android-chrome-192x192.png",
   },
-  manifest: "https://recepies.org/site.webmanifest",
+  manifest: {
+    "name": "Recepies",
+    "short_name": "Recepies",
+    "icons": [
+      {
+        "src": "../public/android-chrome-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "../public/android-chrome-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ],
+    "theme_color": "#ffffff",
+    "background_color": "#ffffff",
+    "display": "standalone"
+  }
 }
 
 export default async function RootLayout({
