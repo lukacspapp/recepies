7d4e9efb2ac3b90a73f40b4b661e31f3c2f7b030
Lukacs J Papp <papplukacs@hotmail.com>
2023-12-07 19:10:13 +0000

    loading state for nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -14,9 +14,11 @@ import {
   DropdownMenu,
   DropdownItem,
   DropdownSection,
+  Spinner,
+  SpinnerProps,
 } from "@nextui-org/react";
 import { usePathname, useRouter } from "next/navigation";
-import { Citrus, LogInIcon, LogOut, UserPlus2 } from "lucide-react";
+import { Citrus, Loader2, LogInIcon, LogOut, SprayCanIcon, UserPlus2 } from "lucide-react";
 import { useAuth } from "@/context/Auth";
 import UserAvatar from "./UserAvatar";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
@@ -24,12 +26,13 @@ import React from 'react';
 import Logo from "./Logo";
 
 const listTypes = [
-  { name: 'Category', href: '/categories', color: 'bg-orange-400' },
-  { name: 'Cusinies', href: '/cuisines', color: 'bg-lime-400' },
+  { name: 'Category', href: '/categories', color: 'bg-orange-400', spinnerColor: 'success' },
+  { name: 'Cusinies', href: '/cuisines', color: 'bg-lime-400', spinnerColor: 'warning' },
 ]
 
 
 export default function Nav() {
+
 
   const pathname = usePathname();
   const router = useRouter();
@@ -37,11 +40,14 @@ export default function Nav() {
 
   const { user } = useAuth()
 
+  const [loading, setLoading] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   async function handleLogout() {
+    setLoading(true)
     const { error } = await supabase.auth.signOut();
     if (!error) router.push('/')
+    setLoading(false)
   }
 
   return (
@@ -54,10 +60,11 @@ export default function Nav() {
             isActive={pathname === item.href}
           >
             <Link
-              className={`text-sm w-[90px] mb-1 font-semibold px-4 py-2  text-gray-700 ${item.color} transform transition-transform hover:scale-105  dark:text-gray-200 rounded-md`}
+              className={`${loading ? 'animate-pulse cursor-progress justify-center' : ''} text-sm w-[90px] mb-1 font-semibold px-4 py-2  text-gray-700 ${item.color} transform transition-transform hover:scale-105  dark:text-gray-200 rounded-md`}
               href={item.href}
+              onClick={() => setLoading(true)}
             >
-              {item.name}
+              {loading ? <Spinner size="sm" color={item.spinnerColor as 'success' | 'warning'} /> : item.name}
             </Link>
           </NavbarMenuItem>
         ))}
@@ -72,10 +79,11 @@ export default function Nav() {
             isActive={pathname === item.href}
           >
             <Link
-              className={`text-sm w-[90px] m-1 font-semibold px-4 py-2  text-gray-700 ${item.color} transform transition-transform hover:scale-105  dark:text-gray-200 rounded-md`}
+              className={`${loading ? 'animate-pulse cursor-progress justify-center' : ''} text-sm w-[90px] m-1 font-semibold px-4 py-2  text-gray-700 ${item.color} transform transition-transform hover:scale-105  dark:text-gray-200 rounded-md`}
               href={item.href}
+              onClick={() => setLoading(true)}
             >
-              {item.name}
+              {loading ? <Spinner size="sm" color={item.spinnerColor as 'success' | 'warning'} /> : item.name}
             </Link>
           </NavbarItem>
         ))}
@@ -108,17 +116,21 @@ export default function Nav() {
                 <p className="font-semibold">Signed in as</p>
                 <p className="font-semibold">{user.email}</p>
               </DropdownItem>
-              <DropdownItem key="help_and_feedback" color="warning" className="py-2">
-                <div className="flex">
-                  <Citrus className="mr-2 h-6 w-6" />
-                  <Link href="/dashboard">
+              <DropdownItem key="help_and_feedback" color="warning" className={`py-2 ${loading ? 'cursor-progress' : ''} `}>
+                <div className="flex items-center">
+                  {loading ? <Loader2 size={15} className='animate-spin mr-2' /> : <Citrus className="mr-2 h-6 w-6" />}
+                  <Link
+                    href="/dashboard"
+                    className={`${loading ? 'cursor-progress' : ''}`}
+                    onClick={() => setLoading(true)}
+                  >
                     Dashboard
                   </Link>
                 </div>
               </DropdownItem>
-              <DropdownItem key="logout" color="danger" className="py-2">
-                <div className="flex">
-                  <LogOut className="mr-2 h-6 w-6" />
+              <DropdownItem key="logout" color="danger" className={`py-2 ${loading ? 'cursor-progress' : ''} `}>
+                <div className="flex items-center">
+                  {loading ? <Loader2 size={15} className='animate-spin mr-2'/> : <LogOut className="mr-2 h-6 w-6" />}
                   <Link onClick={() => handleLogout()}>
                     Logout
                   </Link>
