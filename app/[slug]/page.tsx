571b931cf520cf39cfbc6327391ab65a85149d61
Lukacs J Papp <papplukacs@hotmail.com>
2023-12-08 13:21:27 +0000

    refactor is done

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -15,32 +15,32 @@ export default async function Page({ params }: { params: { slug: string } }) {
     <section className="w-full py-4 md:py-6 lg:py-12 xl:py-24 p-6">
       <div className="container px-2 md:px-4 lg:px-6">
         <div className="grid gap-4 md:gap-6 lg:gap-10 xl:gap-16 md:grid-cols-1 lg:grid-cols-2">
-          <div className="space-y-6 md:space-y-4">
+          <div className="md:space-y-4">
             <Image
               src={meals[0].strMealThumb}
               alt={meals[0].strMeal}
               width={500}
               height={500}
-              className="rounded-xl"
+              className="rounded-xl mb-4"
               priority
             />
-            <h2 className="leading-tighter text-center text-xl font-bold tracking-tighter sm:text-2xl md:text-3xl lg:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
+            <h2 className="leading-tighter mb-2 text-center text-xl font-bold tracking-tighter sm:text-2xl md:text-3xl lg:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
               {meals[0].strMeal}
-            <div className="flex justify-center space-x-2 md:space-x-4 text-sm md:text-base lg:text-lg dark:text-zinc-400">
-              <Badge value={meals[0].strCategory} type='categories' />
-              <Badge value={meals[0].strArea} className="ml-2" type='cuisines' />
-            </div>
             </h2>
-            <h3 className="leading-tight text-lg font-bold tracking-tighter md:text-lg lg:text-xl xl:text-2xl">
+              <div className="flex mt-1 justify-center space-x-2 md:space-x-4 text-sm md:text-base lg:text-lg dark:text-zinc-400">
+                <Badge value={meals[0].strCategory} type='categories' />
+                <Badge value={meals[0].strArea} className="ml-2" type='cuisines' />
+              </div>
+            <h3 className="leading-tight text-lg mt-4 font-bold tracking-tighter md:text-lg lg:text-xl xl:text-2xl">
               Instructions
             </h3>
-            <p className="max-w-[700px] leading-[1.4rem] text-zinc-500 text-sm md:text-base lg:text-lg dark:text-zinc-400">
+            <p className="max-w-[700px] mt-3 leading-[1.4rem] text-zinc-500 text-sm md:text-base lg:text-lg dark:text-zinc-400">
               {meals[0].strInstructions}
             </p>
-            <h3 className="leading-tight text-md font-bold tracking-tighter md:text-lg lg:text-xl xl:text-2xl">
+            <h3 className="leading-tight mt-4 text-md font-bold tracking-tighter md:text-lg lg:text-xl xl:text-2xl">
               Ingredients and Measurements
             </h3>
-            <ul className="list-disc list-inside text-sm md:text-base lg:text-lg dark:text-zinc-400">
+            <ul className="list-disc list-inside mt-3 text-sm md:text-base lg:text-lg dark:text-zinc-400">
               {ingredients.length > 0 ? ingredients.map((ingredient, index) => (
                 <li key={index}>
                   {ingredient} - {measures[index]}
@@ -48,7 +48,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
               )) : null}
             </ul>
           </div>
-          <div className="space-y-2 md:space-y-4">
+          <div className="space-y-4 md:space-y-4 mt-4">
             <h2 className="leading-tighter text-lg font-bold tracking-tighter md:text-xl lg:text-2xl xl:text-3xl">
               Preparation Video
             </h2>
